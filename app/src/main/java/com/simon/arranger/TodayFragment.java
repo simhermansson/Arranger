0a26812ec3f1bca36629db8b0bbaa1f4de509db6
Simon Hermansson <simhe502@student.liu.se>
2019-08-06 17:05:02 +0200

    The input dialog now displays correctly directly above the soft keyboard. Basic information gathering from the edittext fields in the dialog is also retrieved but not yet handled.

--- app/src/main/java/com/simon/arranger/TodayFragment.java
+++ app/src/main/java/com/simon/arranger/TodayFragment.java
@@ -1,24 +1,23 @@
 package com.simon.arranger;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ListView;
-
 import com.simon.arranger.listview_adapters.TaskAdapter;
 import com.simon.arranger.objects.Task;
-
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -49,11 +48,12 @@ public class TodayFragment extends Fragment {
         listView.setAdapter(taskAdapter);
 
         //Handle the floating action button and the popup input bar
-        FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton);
+        final FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openInputDialog();
+                floatingActionButton.hide();
+                openInputDialog(floatingActionButton);
             }
         });
 
@@ -66,11 +66,45 @@ public class TodayFragment extends Fragment {
         activity = (AppCompatActivity) context;
     }
 
-    private void openInputDialog() {
-        Dialog registerDialog = new Dialog(activity);
-        registerDialog.setContentView(R.layout.input_dialog);
-        registerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        registerDialog.show();
+    private void openInputDialog(final FloatingActionButton floatingActionButton) {
+        final Dialog inputDialog = new Dialog(activity, R.style.InputDialogTheme);
+        inputDialog.setContentView(R.layout.input_dialog);
+        WindowManager.LayoutParams params = inputDialog.getWindow().getAttributes();
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.gravity = Gravity.BOTTOM;
+        inputDialog.getWindow().setAttributes(params);
+        inputDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        inputDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                //Close keyboard
+                InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
+                floatingActionButton.show();
+            }
+        });
+        inputDialog.show();
+
+        //Open keyboard
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+        //Handle dialog inputs
+        final EditText inputDialogEditTaskName = inputDialog.findViewById(R.id.inputTaskName);
+        final EditText inputDialogEditTaskTime = inputDialog.findViewById(R.id.inputTaskTime);
+        AppCompatImageButton inputDialogImageButton = inputDialog.findViewById(R.id.inputImageButton);
+        inputDialogImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taskName = inputDialogEditTaskName.getText().toString();
+                String taskTime = inputDialogEditTaskTime.getText().toString();
+                System.out.println(taskName);
+                System.out.println(taskTime);
+                inputDialog.cancel();
+            }
+        });
     }
 
 }
