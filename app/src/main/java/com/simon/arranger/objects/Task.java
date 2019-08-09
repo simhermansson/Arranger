8d4934bcc195de71ed77ddad4fe9d6f2bc8e8e6b
Simon Hermansson <simhe502@student.liu.se>
2019-08-09 13:26:28 +0200

    Improved and simplified the parsing of input.

--- app/src/main/java/com/simon/arranger/objects/Task.java
+++ app/src/main/java/com/simon/arranger/objects/Task.java
@@ -3,6 +3,8 @@ package com.simon.arranger.objects;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task {
     private String name;
@@ -32,54 +34,43 @@ public class Task {
     }
 
     private void parseInput(String input) {
-        //StringBuilders for name, time and when
-        StringBuilder name = new StringBuilder();
-        StringBuilder when = new StringBuilder();
+        try {
+            //Regex Strings
+            String HH = "(0[0-9]|1[0-9]|2[0-9]|[0-9])";
+            String MM = "([:.,][0-5][0-9])";
+            String HHMM = "(" + HH + MM + ")";
+            String AM = "(" + HH + MM + "?\\s?[Aa][Mm])";
+            String PM = "(" + HH + MM + "?\\s?[Pp][Mm])";
+            String TIMES = "(" + HHMM + "|" + AM + "|" + PM + "|" + HH + ")";
+            String SPLITNUMALP = "[AaPp]";
+            String SENTENCE = "^(.+)(at\\s+)" + TIMES + "(.*)";
 
-        for (String word : input.split(" ")) {
-            switch (currentInterpretState) {
-                case NAME:
-                    if (!"at".equals(word)) {
-                        name.append(word);
-                    } else {
-                        this.name = name.toString();
-                        currentInterpretState = InterpretState.TIME;
-                    }
-                    break;
-                case TIME:
-                    try {
-                        //Regex Strings
-                        String HHMM = "^(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]$";
-                        String AM = "^(0[0-9]|1[0-9]|[0-9])[Aa][Mm]$";
-                        String PM = "^(0[0-9]|1[0-9]|[0-9])[Pp][Mm]$";
-                        String HOUR = "^(0[0-9]|1[0-9]|[0-9])$";
-                        String SPLITNUMALP = "[AaPp]";
-
-                        //Check if the word matches any time format
-                        if (word.matches(HHMM)) {
-                            this.time = word;
-                            date = new SimpleDateFormat("hh:mm").parse(word);
-                        } else if (word.matches(AM)) {
-                            this.time = word.split(SPLITNUMALP)[0];
-                            date = new SimpleDateFormat("hh").parse(word.split(SPLITNUMALP)[0]);
-                        } else if (word.matches(PM)) {
-                            this.time = word.split(SPLITNUMALP)[0];
-                            date = new SimpleDateFormat("hh").parse(word.split(SPLITNUMALP)[0]);
-                        } else if (word.matches(HOUR)) {
-                            this.time = word.split(SPLITNUMALP)[0];
-                            date = new SimpleDateFormat("hh").parse(word.split(SPLITNUMALP)[0]);
-                        } else {
-                            this.time = "Woops";
-                        }
-
-                    } catch (ParseException e) {
-                        System.out.println(e.toString());
-                    }
-                    currentInterpretState = InterpretState.WHEN;
-                    break;
-                case WHEN:
-                    break;
+            Pattern pattern = Pattern.compile(SENTENCE);
+            Matcher matcher = pattern.matcher(input);
+            while (matcher.find()) {
+                name = matcher.group(1);
+                time = matcher.group(3);
             }
+
+            //Parse the time format
+            if (time.matches(HHMM)) {
+                this.time = time;
+                date = new SimpleDateFormat("hh:mm").parse(time);
+            } else if (time.matches(AM)) {
+                this.time = time.split(SPLITNUMALP)[0];
+                date = new SimpleDateFormat("hh").parse(time.split(SPLITNUMALP)[0]);
+            } else if (time.matches(PM)) {
+                this.time = time.split(SPLITNUMALP)[0];
+                date = new SimpleDateFormat("hh").parse(time.split(SPLITNUMALP)[0]);
+            } else if (time.matches(HH)) {
+                this.time = time.split(SPLITNUMALP)[0];
+                date = new SimpleDateFormat("hh").parse(time.split(SPLITNUMALP)[0]);
+            } else {
+                this.time = "Woops";
+            }
+
+        } catch (ParseException e) {
+            System.out.println(e.toString());
         }
     }
 }
